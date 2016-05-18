1782306ccc70c4b900520860f541f2486378d3b4
Draveness <stark.draven@gmail.com>
2016-05-18 22:03:52 +0800

    Removed pods from work space

--- Example/Pods/Target Support Files/OKObserver/OKObserver-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OKObserver : NSObject
-@end
-@implementation PodsDummy_OKObserver
-@end
--- Example/Pods/Target Support Files/Pods-OKObserver_Tests/Pods-OKObserver_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OKObserver_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_OKObserver_Tests
-@end
